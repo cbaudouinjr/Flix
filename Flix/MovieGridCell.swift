b217f84230cb117ff1468f9f81a44b53e81a3eaf
Chris Baudouin, Jr <chrisbaudouinjr@icloud.com>
2019-10-25 20:37:04 -0400

    Flix Part 2 - Adds tab bar layout - Adds superhero section

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Christopher Baudouin, Jr. on 10/24/19.
+//  Copyright © 2019 Christopher Baudouin, Jr. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
